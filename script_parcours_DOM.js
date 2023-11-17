d1eb94256ab8c71c206158c77b2ddf4517a6afd5
ikramiste <ikrame.saadi@gmail.com>
2023-11-17 15:46:36 +0100

    const with console logs - delete console logs later

---
+++ script_parcours_DOM.js
@@ -0,0 +1,8 @@
+const paragraphs = document.getElementsByTagName('p');
+console.log(paragraphs)
+
+const hello = document.getElementById('coucou');
+console.log(hello)
+
+const thirdUrl = document.getElementsByTagName('a')[2];
+console.log(thirdUrl)
